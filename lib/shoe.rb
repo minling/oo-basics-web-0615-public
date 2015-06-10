45f9151277b4e08af27de7ae5472243d6ec3f2b2
minling <mzhao725@gmail.com>
2015-06-09 23:46:53 -0400

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :material, :size, :color, :condition 
+  attr_reader :brand 
+
+  BRANDS =[]
+
+  def initialize(brand)
+    @brand = brand
+    if BRANDS.include?(brand)  #if the array doesn't have that  particular brand, shovel it in 
+    else
+      BRANDS << brand
+    end
+  end
+
+  def cobble
+    @condition = "new" 
+    puts "Your shoe is as good as new!"
+  end
+
+end
